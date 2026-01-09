c63a28299ddda8ddb2c50739320aa598c7c4465a
Milos Mirkovic <milosdmirkovic@gmai.com>
2026-01-09 15:51:25 +0100

    feat: add users controller with getUsers and createUser

---
+++ src/controllers/users.controller.js
@@ -0,0 +1,30 @@
+import User from "../models/User.js";
+
+const getusers = async (req,res,next)=>{
+    try{
+        const users = await User.find();
+        res.json(users);
+    }catch(err){
+        next(err);
+    }
+};
+
+const createUser = async (req,res,next)=>{
+    try{
+        const {name,email}= req.body;
+        if(!name || !email){
+            const error = new Error("name and email are required");
+            error.status=400;
+            return next(error);
+        };
+        const newUser = await User.create({name,email});
+        res.status(201).json({
+            message:"New user created",
+            user:newUser
+        });
+    }catch(err){
+        next(err);
+    }
+};
+
+export { getUsers, createUser };
